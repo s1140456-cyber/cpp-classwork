38005da8cc0dd24c255e89d4ac36736e67c4fb9f
s1140456-cyber <s1140456@dmhs.kh.edu.tw>
2026-05-15 11:30:20 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age; 
+    string name;
+    cout<< "請輸入年齡"<< endl;
+	cin >> age; 
+    cout <<"請輸入名字" <<endl;
+	cin >> name;
+	cout<<"你是" <<"今年"<< age << age << "歲" << endl; 
+ 
+}
