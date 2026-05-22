995d62455ee5ff7599312af0ea2ab0176a1aa0d3
s1140456-cyber <s1140456@dmhs.kh.edu.tw>
2026-05-22 10:58:55 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+imt main(){
+    cout << "1.趙建勝帥哥" << endl ; e
+    cout << "2.趙建勝 帥哥" << endl ; e
+    cout << "3.趙建勝\t帥哥" << endl ; e
+    cout << "4.趙建勝\n帥哥" << endl ; e
+    cout << "\"5.趙建勝帥哥\"'<< endl ; e
+    cout << "\6.'趙建勝帥哥'" << endl ; e
+    cout << "\\7.趙建勝帥哥\\" << endl ; e
+    return 0;
+    
